84cbaadf976df5aeb5aff77eecfab2dab2e6df73
Elad Tal <eladt@microsoft.com>
2020-06-01 15:39:18 +0300

    enum

---
+++ .history/app/verdaccio/installation-status_20200225120246.ts
@@ -0,0 +1,11 @@
+export enum InstallationStatus {
+    pending        = 0,
+    registered  = 1 << 0, // 00001
+    in_process  = 1 << 1, // 00010
+    warnings    = 1 << 2, // 00100
+    success     = 1 << 3, // 01000
+    error       = 1 << 4, // 10000
+    completed   = InstallationStatus.success | InstallationStatus.error,
+    all = ~(~0 << 4) // 1111
+    // tslint:enable:no-bitwise
+}
